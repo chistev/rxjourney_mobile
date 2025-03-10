d959e8b7233f291a930351050e1fa40bef642ba1
chistev <stephenowabie@gmail.com>
2025-03-10 11:14:29 +0100

    added home and about icons

--- components/MainTabs.js
+++ components/MainTabs.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
 import Navbar from "../components/Navbar";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,7 +11,22 @@ export default function MainTabs() {
   return (
     <View style={styles.container}>
       <Navbar />
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "home-outline";
+            } else if (route.name === "About") {
+              iconName = "information-circle-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="About" component={AboutScreen} />
       </Tab.Navigator>
